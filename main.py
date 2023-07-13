03cd90a6f43e26a1222e5a2f1def58083c1da826
Calvin Roberts <roberts.calvin85@gmail.com>
2023-07-14 00:08:23 +0100

    Flask Flash Messages

--- main.py
+++ main.py
@@ -37,22 +37,23 @@ def home():
 def register():
     if request.method == "POST":
 
+        if User.query.filter_by(email=request.form.get('email')).first():
+            #User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
         hash_and_salted_password = generate_password_hash(
             request.form.get('password'),
             method='pbkdf2:sha256',
             salt_length=8
         )
-
         new_user = User(
             email=request.form.get('email'),
             name=request.form.get('name'),
             password=hash_and_salted_password,
         )
-
         db.session.add(new_user)
         db.session.commit()
-
-        #Log in and authenticate user after adding details to database.
         login_user(new_user)
 
         return redirect(url_for("secrets"))
@@ -66,11 +67,17 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         
-        #Find user by email entered.
         user = User.query.filter_by(email=email).first()
-        
-        #Check stored password hash against entered password hashed.
-        if check_password_hash(user.password, password):
+        #Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        #Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        #Email exists and password correct
+        else:
             login_user(user)
             return redirect(url_for('secrets'))
           
